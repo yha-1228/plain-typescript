3eab2b8ae28e9c44c91523e503d32e65c787b856
yuta <gssbtsm999@gmail.com>
2023-12-17 07:47:54 +0900

    docs(groupBy): remove docs

--- src/groupBy.ts
+++ src/groupBy.ts
@@ -1,30 +1,3 @@
-/**
- * オブジェクトの配列を特定のキーでグルーピングする。
- *
- * @example
- * ```ts
- * const menus = [
- *   { id: 1, type: 'meat', name: '牛丼' },
- *   { id: 2, type: 'fish', name: '海鮮丼' },
- *   { id: 3, type: 'meat', name: '牛皿' },
- *   { id: 4, type: 'fish', name: '刺身' },
- * ];
- *
- * const grouped = groupBy(menus, 'type');
- * // output:
- * //
- * // {
- * //   meat: [
- * //     { id: 1, type: 'meat', name: '牛丼' },
- * //     { id: 3, type: 'meat', name: '牛皿' }
- * //   ],
- * //   fish: [
- * //     { id: 2, type: 'fish', name: '海鮮丼' },
- * //     { id: 4, type: 'fish', name: '刺身' }
- * //   ]
- * // }
- * ```
- */
 function groupBy<T extends Record<string, unknown>>(
   array: ArrayLike<T>,
   key: keyof T,
