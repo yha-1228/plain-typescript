6b9a9f3b068462af806af86397ca974beff5630c
yuta <gssbtsm999@gmail.com>
2023-12-17 13:10:25 +0900

    docs(groupBy): add example

--- src/groupBy.ts
+++ src/groupBy.ts
@@ -1,3 +1,31 @@
+/**
+ * @example
+ * ```ts
+ * const frameworks = [
+ *   { id: 1, name: 'CakePHP', language: 'PHP' },
+ *   { id: 2, name: 'Larabel', language: 'PHP' },
+ *   { id: 3, name: 'React', language: 'JavaScript' },
+ *   { id: 4, name: 'Angular', language: 'JavaScript' },
+ *   { id: 5, name: 'Vue', language: 'JavaScript' },
+ *   { id: 6, name: 'Bootstrap', language: 'CSS' },
+ * ];
+ *
+ * groupBy(frameworks, ({ language }) => language);
+ *
+ * // {
+ * //   PHP: [
+ * //     { id: 1, name: 'CakePHP', language: 'PHP' },
+ * //     { id: 2, name: 'Larabel', language: 'PHP' }
+ * //   ],
+ * //   JavaScript: [
+ * //     { id: 3, name: 'React', language: 'JavaScript' },
+ * //     { id: 4, name: 'Angular', language: 'JavaScript' },
+ * //     { id: 5, name: 'Vue', language: 'JavaScript' }
+ * //   ],
+ * //   CSS: [ { id: 6, name: 'Bootstrap', language: 'CSS' } ]
+ * // }
+ * ```
+ */
 function groupBy<T extends object, U extends string>(
   array: T[],
   callbackFn: (item: T) => U,
